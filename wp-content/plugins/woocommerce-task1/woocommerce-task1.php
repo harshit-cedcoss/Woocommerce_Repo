443f474ece1e6dd176603932a2e446af7e8fca29
Harshit-Cedcoss <harshitagarwal@cedcoss.com>
2020-08-14 20:34:24 +0530

    Custom task(my account)

--- wp-content/plugins/woocommerce-task1/woocommerce-task1.php
+++ wp-content/plugins/woocommerce-task1/woocommerce-task1.php
@@ -228,88 +228,192 @@ if ( in_array( 'woocommerce/woocommerce.php', get_option( 'active_plugins' ) ) )
 	}
 	add_filter( 'woocommerce_get_item_data', 'display_product_remark', 10, 2 );
 
-}
 
-// echo '<pre>';
-// var_dump(curl_version());
-// echo '</pre>';
 
-/**
- * Woocommere Mailchimp Integration
- */
-function mailchimp_integration() {
+	// echo '<pre>';
+	// var_dump(curl_version());
+	// echo '</pre>';
 
-	$api_key        = 'b407f3bd7139bc14ff72eaa1ab5a257a-us17';
-	$list_id        = 'abe3b053be';
-	$subsciber_hash = md5( strtolower( $_POST['email'] ) );
-	$data_center    = 'us17';
-	// print_r( $_POST );die;
-	$url  = 'https://' . $data_center . '.api.mailchimp.com/3.0/lists/' . $list_id . '/members/' . $subsciber_hash; // endpoint.
-	$data = array(
-		'email_address' => $_POST['email'],
-		'status'        => 'subscribed', // subscribed, unsubscribed, cleaned, pending.
-		'merge_fields'  => array(
-			'FNAME' => $_POST['username'],
-			'LNAME' => '',
-		),
-		// 'email_address' => 'shantanu12@gmail.com',
-		// 'status'        => 'subscribed',
-		// 'merge_fields'  => array(
-		// 'FNAME' => 'Shantanu',
-		// 'LNAME' => 'Kumar',
-		// ),
-	);
-	$json = json_encode( $data );
-																	// Integration using PHP cURL.
-	/*
-	$ch = curl_init();
-	curl_setopt( $ch, CURLOPT_URL, $url );
-	curl_setopt( $ch, CURLOPT_USERPWD, 'user:' . $api_key );
-	curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json' ) );
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-	curl_setopt( $ch, CURLOPT_TIMEOUT, 10 );
-	curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'PUT' );
-	curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
-	curl_setopt( $ch, CURLOPT_POSTFIELDS, $json );
+	/**
+	 * Woocommere Mailchimp Integration
+	 */
+	function mailchimp_integration() {
 
-	$result = curl_exec( $ch );
-	$http_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+		$api_key        = 'b407f3bd7139bc14ff72eaa1ab5a257a-us17';
+		$list_id        = 'abe3b053be';
+		$subsciber_hash = md5( strtolower( $_POST['email'] ) );
+		$data_center    = 'us17';
+		// print_r( $_POST );die;
+		$url  = 'https://' . $data_center . '.api.mailchimp.com/3.0/lists/' . $list_id . '/members'; // endpoint.
+		$data = array(
+			'email_address' => $_POST['email'],
+			'status'        => 'subscribed', // subscribed, unsubscribed, cleaned, pending.
+			'merge_fields'  => array(
+				'FNAME' => $_POST['username'],
+				'LNAME' => '',
+			),
+			// 'email_address' => 'shantanu12@gmail.com',
+			// 'status'        => 'subscribed',
+			// 'merge_fields'  => array(
+			// 'FNAME' => 'Shantanu',
+			// 'LNAME' => 'Kumar',
+			// ),
+		);
+		$json = json_encode( $data );
+																		// Integration using PHP cURL.
+		/*
+		$ch = curl_init();
+		curl_setopt( $ch, CURLOPT_URL, $url );
+		curl_setopt( $ch, CURLOPT_USERPWD, 'user:' . $api_key );
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json' ) );
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+		curl_setopt( $ch, CURLOPT_TIMEOUT, 10 );
+		curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'PUT' );
+		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
+		curl_setopt( $ch, CURLOPT_POSTFIELDS, $json );
 
-	if ( false === $result ) {
-		echo 'Error Number:' . curl_errno( $ch ) . '<br>';
-		echo 'Error String:' . curl_error( $ch );
+		$result = curl_exec( $ch );
+		$http_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+
+		if ( false === $result ) {
+			echo 'Error Number:' . curl_errno( $ch ) . '<br>';
+			echo 'Error String:' . curl_error( $ch );
+		}
+
+		curl_close( $ch );
+
+		return $http_code . $result;
+		*/
+																	// Integration using wp_remote_post().
+		$basic_auth = 'Basic ' . base64_encode( 'user:' . $api_key );
+		$options    = array(
+			'method'      => 'POST',
+			'timeout'     => 10,
+			'redirection' => 5,
+			'blocking'    => true,
+			'body'        => $json,
+			'headers'     => array(
+				'Content-Type'  => 'application/json',
+				'Authorization' => $basic_auth,
+			),
+			'cookies'     => array(),
+			'sslverify'   => false,
+		);
+
+		$response = wp_remote_post( $url, $options );
+
+		if ( is_wp_error( $response ) ) {
+			$error_message = $response->get_error_message();
+			echo 'Something went wrong:' . $error_message;
+		} else {
+			echo 'Response:<pre>';
+		//	print_r( $_POST );
+		//	print_r( $response );
+			echo '</pre>';
+		}
 	}
+	//echo mailchimp_integration();
+	add_action( 'user_register', 'mailchimp_integration' );
 
-	curl_close( $ch );
+	$response1 = wp_remote_get( 'https://api.github.com/users/wordpress' );
+	//echo 'Response:<pre>';
+	//print_r( $response1 );
+	$last_modified = wp_remote_retrieve_header( $response1, 'last-modified' );
+	//echo "<br>";
+	//echo $last_modified;
+	//echo '</pre>';
 
-	return $http_code . $result;
-	*/
-																		// Integration using wp_remote_post().
-	$basic_auth = 'Basic ' . base64_encode( 'user:' . $api_key );
-	$options    = array(
-		'method'      => 'PUT',
-		'timeout'     => 10,
-	//	'redirection' => 5,
-	//	'blocking'    => true,
-		'body'        => $json,
-		'headers'     => array(
-			'Content-Type'  => 'application/json',
-			'Authorization' => $basic_auth,
-		),
-		'cookies'     => array(),
-		'sslverify'   => false,
-	);
+	/**
+	 * Step 1. Add Link (Tab) to My Account menu
+	 */
+	function last_order_link( $items ) {
 
-	$response = wp_remote_post( $url, $options );
+		$items = array_slice( $items, 0, 4, true ) 
+		+ array( 'last-order' => 'Last Order' )
+		+ array_slice( $items, 4, 2, true );
+		//$menu_links['last-order'] = __( 'Last Order', '' );
 
-	if ( is_wp_error( $response ) ) {
-		$error_message = $response->get_error_message();
-		echo 'Something went wrong:' . $error_message;
-	} else {
-		echo 'Response:<pre>';
-		print_r( $response );
-		echo '</pre>';
+		return $items;
+
 	}
+	add_filter( 'woocommerce_account_menu_items', 'last_order_link', 40 );
+	/**
+	 * Step 2. Register Permalink Endpoint
+	 */
+	function add_endpoint_last_order() {
+
+		add_rewrite_endpoint( 'last-order', EP_PAGES );
+
+	}
+	add_action( 'init', 'add_endpoint_last_order' );
+	/**
+	 * Step 3. Content for the new page in My Account
+	 */
+	function my_account_endpoint_content() {
+
+		if ( is_user_logged_in() ) {
+
+			$user_id  = get_current_user_id();
+			$customer = new WC_Customer( $user_id );
+
+			$last_order = $customer->get_last_order();
+			$item_count = $last_order->get_item_count() - $last_order->get_item_count_refunded();
+			//print_r( $last_order );
+			if ( $last_order ) {
+				//die("die");
+				?>
+				<table class="woocommerce-orders-table woocommerce-MyAccount-orders shop_table shop_table_responsive my_account_orders account-orders-table" >
+					<thead>
+						<tr>
+							<?php foreach ( wc_get_account_orders_columns() as $column_id => $column_name ) { ?>
+								<th class="woocommerce-orders-table__header woocommerce-orders-table__header-<?php echo esc_attr( $column_id ); ?>"><span class="nobr"><?php echo esc_html( $column_name ); ?></span></th>
+							<?php } ?>
+						</tr>
+					</thead>
+
+					<tbody>
+						<tr class="woocommerce-orders-table__row woocommerce-orders-table__row--status-<?php echo esc_attr( $last_order->get_status() ); ?> order">
+							<?php foreach ( wc_get_account_orders_columns() as $column_id => $column_name ) { ?>
+								<td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-<?php echo esc_attr( $column_id ); ?>" data-title="<?php echo esc_attr( $column_name ); ?>" >
+									<?php if ( has_action( 'woocommerce_my_account_my_orders_column_' . $column_id ) ) : ?>
+									<?php //do_action( 'woocommerce_my_account_my_orders_column_' . $column_id, $order ); ?>
+
+									<?php elseif ( 'order-number' === $column_id ) : ?>
+										<a href="<?php echo esc_url( $last_order->get_view_order_url() ); ?>">
+											<?php echo esc_html( _x( '#', 'hash before order number', 'woocommerce' ) . $last_order->get_order_number() ); ?>
+										</a>
+
+									<?php elseif ( 'order-date' === $column_id ) : ?>
+										<time datetime="<?php echo esc_attr( $last_order->get_date_created()->date( 'c' ) ); ?>"><?php echo esc_html( wc_format_datetime( $last_order->get_date_created() ) ); ?></time>
+
+									<?php elseif ( 'order-status' === $column_id ) : ?>
+										<?php echo esc_html( wc_get_order_status_name( $last_order->get_status() ) ); ?>
+
+									<?php elseif ( 'order-total' === $column_id ) : ?>
+										<?php
+										/* translators: 1: formatted order total 2: total order items */
+										echo wp_kses_post( sprintf( _n( '%1$s for %2$s item', '%1$s for %2$s items', $item_count, 'woocommerce' ), $last_order->get_formatted_order_total(), $item_count ) );
+										?>
+
+									<?php elseif ( 'order-actions' === $column_id ) : ?>
+										<?php
+										$actions = wc_get_account_orders_actions( $last_order );
+
+										if ( ! empty( $actions ) ) {
+											foreach ( $actions as $key => $action ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.OverrideProhibited
+												echo '<a href="' . esc_url( $action['url'] ) . '" class="woocommerce-button button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
+											}
+										}
+										?>
+									<?php endif; ?>
+								</td>
+							<?php } ?>
+						</tr>
+					</tbody>
+				</table>
+				<?php
+			}
+		}
+	}
+	add_action( 'woocommerce_account_last-order_endpoint', 'my_account_endpoint_content' );
 }
-//echo mailchimp_integration();
-add_action( 'user_register', 'mailchimp_integration' );
